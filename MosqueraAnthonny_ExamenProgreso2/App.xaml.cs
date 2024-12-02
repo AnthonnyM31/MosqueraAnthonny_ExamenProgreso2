2184816fa55ce35f7fb7f71bdd22f94be8771a90
laboratorio <laboratorio@UPOAULA10201>
2024-12-02 07:17:22 -0500

    Agregar archivos de proyecto.

---
+++ MosqueraAnthonny_ExamenProgreso2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MosqueraAnthonny_ExamenProgreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
